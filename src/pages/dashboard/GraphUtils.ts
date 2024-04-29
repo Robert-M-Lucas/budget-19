eb4e736231d102fa54a27e26a4fe93e04d1b78da
joesumargo <joesumargo@gmail.com>
2024-04-29 01:10:27 +0100

    Attempted ToDo #2 TO DO: 2. Fix the graph because moneyIn and MoneyOut is the similar 3. Finally, add the proper expenses, income, savings graphs

--- src/pages/dashboard/GraphUtils.ts
+++ src/pages/dashboard/GraphUtils.ts
@@ -5,16 +5,14 @@ export type transactionPoint = { date: string; amount: number; goal: number }
 export type graphData = {points: transactionPoint[][], title: string}
 export type finalGraphData = {raw: graphData, in: graphData, out: graphData};
 
-const splitCumulateByMonth = (points: transactionPoint[]): transactionPoint[][] => {
+const splitByMonth = (points: transactionPoint[]): transactionPoint[][] => {
     const result: transactionPoint[][] = [];
     let currMonth: string = "";
     let currArr: transactionPoint[] = [];
-
     // Split
     points.forEach( p => {
         const [day,month, year] = p.date.split('/').map(Number);
         const dateKey = `${month}/${year}`;
-
         if (dateKey !== currMonth) {
             if (currArr.length) {
                 result.push(currArr);
@@ -27,19 +25,25 @@ const splitCumulateByMonth = (points: transactionPoint[]): transactionPoint[][] 
     if (result.length) {
         result.push(currArr);
     }
-
-    // Cumulate
-    result.forEach(month => { cumulateTransactions(month) })
+    // // Cumulate
+    // if (doCumulate) { result.forEach(month => { cumulateTransactions(month) }); console.log("DO CUMULATE",result); return result;}
+    // console.log("DONOT CUMULATE",result)
     return result;
 }
 
-function cumulateTransactions(points: transactionPoint[]): transactionPoint[] {
-    let total = 0;
-    return points.map(value => {
-        total += value.amount;
-        value.amount = total;
-        return value;
+function cumulateTransactions(points: transactionPoint[][]): transactionPoint[][] {
+    points.map(month => {
+        let total = 0;
+        return month.map(t => {
+            total += t.amount;
+            return {
+                date: t.date,
+                amount: total,
+                goal: t.goal
+            };
+        })
     })
+    return points
 }
 
 function getDateString(timestamp: number): string  {
@@ -56,10 +60,17 @@ function splitTransactions (data: transactionPoint[]): finalGraphData {
             moneyOut.push(t)
         }
     })
-
-    return {raw: {points: splitCumulateByMonth(data), title: "Raw"},
-            in: {points: splitCumulateByMonth(moneyIn), title: "Income"},
-            out: {points: splitCumulateByMonth(moneyOut), title: "Expenses"}};
+    // Splitting the points
+    const splitedData: transactionPoint[][][] = [splitByMonth(data), splitByMonth(moneyIn), splitByMonth(moneyOut)]
+    const cumulatedSData: transactionPoint[][][] = [cumulateTransactions(splitedData[0]),
+                                                    cumulateTransactions(splitedData[1]),
+                                                    cumulateTransactions(splitedData[2])
+                                                ]
+    console.log(splitedData)
+    console.log(cumulatedSData)
+    return {raw: {points: cumulatedSData[0], title: "Balance"},
+            in: {points: cumulatedSData[1], title: "Income"},
+            out: {points: cumulatedSData[2], title: "Expenses"}};
 }
 
 export function readTransactions(data: Transaction[]): finalGraphData {
